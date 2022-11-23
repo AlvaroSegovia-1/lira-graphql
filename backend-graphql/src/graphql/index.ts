67723be18cfcff399f97955ae8b6258a7497f462
Alvagit25 <alvarodesegovia@gmail.com>
2022-11-23 14:47:12 +0100

    añadir resolvers

--- backend-graphql/src/graphql/index.ts
+++ backend-graphql/src/graphql/index.ts
@@ -3,11 +3,14 @@ import {GraphQLSchema} from 'graphql'
 import { makeExecutableSchema } from '@graphql-tools/schema'
 import 'graphql-import-node' 
 import rootSchema from './schemas/schema.graphql'
+import resolvers from '../graphql/schemas/resolvers/resolversMaps'
 
 export const schema = makeExecutableSchema({ 
     typeDefs:[
         rootSchema
-] })
+    ],
+    resolvers
+ })
 
 /* export const schema: GraphQLSchema = makeExecutableSchema({
     typeDefs: [
